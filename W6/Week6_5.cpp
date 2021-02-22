43592df81724954ee8764730d5331092400815f5
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2021-02-22 22:28:26 +0900

    Add files via upload

---
+++ W6/Week6_5.cpp
@@ -0,0 +1,19 @@
+//Summary of W6
+//
+//1. initializing: =
+//2. Concatenating/Reading: +, getline(cin, str)
+//3. indexing: str[i]
+//4. Slicing: str.substr(ind, len)
+//5. Length: str.length()
+//6. Comparing: ==, !=, <, >, <=, >=
+//7. Searching: str.find(s), str.find(s, startingPoint)
+
+
+
+
+
+
+
+
+
+
