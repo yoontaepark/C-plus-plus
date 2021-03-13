48267c55660168adf011d03e1a80fda4274787e0
yoontaepark <65579629+yoontaepark@users.noreply.github.com>
2021-03-13 23:03:16 +0900

    Add files via upload

---
+++ W8/yp2201_hw8_q1.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+void printTriangle(int n);
+void printOpositeTriangles(int n);
+void printRuler(int n);
+
+int main()
+{
+	printTriangle(4);
+	cout << endl;
+	printOpositeTriangles(4);
+	cout << endl;
+	printRuler(4);
+
+
+	return 0;
+}
+
+void printTriangle(int n)
+{
+	if (n <= 0)
+		return;
+	else
+	{
+		printTriangle(n - 1);
+		for (int i = 0; i < n; i++)
+			cout << "*";
+	}
+	cout << endl;
+}
+
+
+void printOpositeTriangles(int n)
+{
+	if (n <= 0)
+		return;
+	else
+	{
+		for (int i = 0; i < n; i++)
+			cout << "*";
+		cout << endl;
+	
+		printOpositeTriangles(n - 1);
+		
+		for (int i = 0; i < n; i++)
+			cout << "*";
+	}
+	cout << endl;
+}
+
+void printRuler(int n)
+{
+	if (n <= 0)
+		return;
+	else
+	{
+		printRuler(n - 1);
+		for (int i = 0; i < n; i++)
+			cout << "-";
+		cout << endl;
+		printRuler(n - 1);
+	}
+}
+
